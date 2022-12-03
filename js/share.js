f6b271da6760204b25ad111b5be6fbfd9f734419
AdiSemper <semperag@dukes.jmu.edu>
2022-12-03 15:19:06 -0500

    playing on iphone battery low

--- js/share.js
+++ js/share.js
@@ -7,6 +7,24 @@
                 
             });
 
+            Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+                get: function () {
+                    return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+                }
+            });
+
+            $('body').on('click touchstart', function () {
+                const videoElement = document.getElementById('preloader');
+                if (videoElement.playing) {
+                    // video is already playing so do nothing
+                }
+                else {
+                    // video is not playing
+                    // so play video now
+                    videoElement.play();
+                }
+            });
+
             $.get("https://semperag.github.io/nav.html", function(data){
             $("#menu").on('click',function(){
                 document.getElementById("topnav-right").classList.toggle('active');
