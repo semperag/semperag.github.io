adaf429fee5341511b8aca991fa0fe3385f67a40
AdiSemper <semperag@dukes.jmu.edu>
2023-03-05 03:24:30 -0500

    make resize menu work properly

--- js/share.js
+++ js/share.js
@@ -4,13 +4,12 @@
             window.addEventListener("load", function() {
                 console.log(loader);
                 setTimeout(() => {  loader.style.display = "none"; }, 500);
-            });
 
-            $.get("https://semperag.github.io/nav.html", function(data){
-            $("#menu").on('click',function(){
-                document.getElementById("topnav-right").classList.toggle('active');
-            });
-
+                $.get("https://semperag.github.io/nav.html", function(data){
+                    $("#menu").on('click',function(){
+                        document.getElementById("topnav-right").classList.toggle('active');
+                    });
+                });
             });
             
             $(window).resize(function(){
