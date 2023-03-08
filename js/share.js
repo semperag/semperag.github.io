a2b7c0bb442e8c032b8c56c2689e73cd11afdfa7
AdiSemper <semperag@dukes.jmu.edu>
2023-03-07 23:14:24 -0500

    unnecessary log and space

--- js/share.js
+++ js/share.js
@@ -1,22 +1,21 @@
 
-            var loader = document.getElementById("preloader");   
+var loader = document.getElementById("preloader");   
 
-            window.addEventListener("load", function() {
-                console.log(loader);
-                setTimeout(() => {  loader.style.display = "none"; }, 500);
+window.addEventListener("load", function() {
+    setTimeout(() => {  loader.style.display = "none"; }, 500);
 
-                $.get("https://semperag.github.io/nav.html", function(data){
-                    $("#menu").on('click',function(){
-                        document.getElementById("topnav-right").classList.toggle('active');
-                    });
-                });
-            });
-            
-            $(window).resize(function(){
+    $.get("https://semperag.github.io/nav.html", function(data){
+        $("#menu").on('click',function(){
+            document.getElementById("topnav-right").classList.toggle('active');
+        });
+    });
+});
 
-                if(window.innerWidth < 851) {
-                    $.get("https://semperag.github.io/nav.html", function(data){
-                    $("#topnav-right").removeClass('active');
-                    });
-                }
-            });
+$(window).resize(function(){
+
+    if(window.innerWidth < 851) {
+        $.get("https://semperag.github.io/nav.html", function(data){
+        $("#topnav-right").removeClass('active');
+        });
+    }
+});
